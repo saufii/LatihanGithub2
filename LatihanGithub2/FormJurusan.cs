435233d1aea79f928da38b5a731f2558979981e3
khairudin86 <bocilsamba@gmail.com>
2025-10-14 14:57:35 +0800

    ok khairudin

---
+++ LatihanGithub2/FormJurusan.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LatihanGithub2
+{
+    public partial class FormJurusan : Form
+    {
+        public FormJurusan()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
