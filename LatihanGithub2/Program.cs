7df71610caedce42b9a4859de75d9cfb5c633c6a
Azelzyyy <mazwarrahman398@gmail.com>
2025-10-21 15:05:04 +0800

    Form Siswa dari Azwar

--- LatihanGithub2/Program.cs
+++ LatihanGithub2/Program.cs
@@ -16,7 +16,7 @@ namespace LatihanGithub2
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new FormSiswa());
         }
     }
 }
